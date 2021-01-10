d6739a83cbca998344a5d275fcd7c587597cf6bb
Dini4ka <boss.karelov@gmail.com>
2021-01-10 21:56:20 +0300

    initial commit

---
+++ instabot.py
@@ -0,0 +1,127 @@
+# нужна программа, которая будет автоматически отправлять комментарии на определённые посты.
+# должна быть возможность указать ссылку на пост,
+# задать различный текст комментариев и временной интервал отправки (каждый час, каждые 7 мин и т.п.)
+
+# login1 : dorozhka_ula
+# number_phone1 : 79641411590
+# password1 : Denis_71265
+
+# login2 : petranna12
+# number_phone2 : 79605908231
+# password2 : Denis_71265
+
+# login3 : andrpan12
+# number_phone3 : 79258688784
+# password3 : Denis_71265
+
+# link : https://www.instagram.com/p/CJkAmg-h43m/
+
+from time import sleep
+import pickle
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import random
+
+# Запуск всех трёх ботов
+bot1 = webdriver.Firefox(executable_path='geckodriver.exe')
+bot2 = webdriver.Firefox(executable_path='geckodriver.exe')
+bot3 = webdriver.Firefox(executable_path='geckodriver.exe')
+
+
+class instabot:
+
+    def __init__(self):
+        self.username_bot1 = 'dorozhka_ula'
+        self.password_bot1 = 'Denis_71265'
+        self.username_bot2 = 'petranna12'
+        self.password_bot2 = 'Denis_71265'
+        self.username_bot3 = 'andrpan12'
+        self.password_bot3 = 'Denis_71265'
+
+        #авторизуем ботов
+        bot1.get('https://www.instagram.com/')
+        bot2.get('https://www.instagram.com/')
+        bot3.get('https://www.instagram.com/')
+        sleep(2)
+
+        # Следующие строки говорят боту найти поля для заполнения логина и пароля.
+        username_input_bot1 = bot1.find_element_by_name("username")
+        password_input_bot1 = bot1.find_element_by_name("password")
+        username_input_bot2 = bot2.find_element_by_name("username")
+        password_input_bot2 = bot2.find_element_by_name("password")
+        username_input_bot3 = bot3.find_element_by_name("username")
+        password_input_bot3 = bot3.find_element_by_name("password")
+
+        # Заполнение логина и пароля
+        username_input_bot1.send_keys(self.username_bot1)
+        password_input_bot1.send_keys(self.password_bot1)
+        username_input_bot2.send_keys(self.username_bot2)
+        password_input_bot2.send_keys(self.password_bot2)
+        username_input_bot3.send_keys(self.username_bot3)
+        password_input_bot3.send_keys(self.password_bot3)
+
+        # Вход
+        login_button_bot1 = bot1.find_element_by_xpath("//button[@type='submit']")
+        login_button_bot2 = bot2.find_element_by_xpath("//button[@type='submit']")
+        login_button_bot3 = bot3.find_element_by_xpath("//button[@type='submit']")
+        try:
+            login_button_bot1.click()
+            print('bot1 is ready')
+        except:
+            print('bot1 is broken')
+        try:
+            login_button_bot2.click()
+            print('bot2 is ready')
+        except:
+            print('bot2 broken')
+        try:
+            login_button_bot3.click()
+            print('bot3 is ready')
+        except:
+            print('bot3 broken')
+        # Берём куки или обновляем их
+        pickle.dump(bot1.get_cookies(), open("cookies_bot_1.pkl", "wb"))
+        pickle.dump(bot2.get_cookies(), open("cookies_bot_2.pkl", "wb"))
+        pickle.dump(bot3.get_cookies(), open("cookies_bot_3.pkl", "wb"))
+
+        sleep(5)
+
+
+    def type_comments(self, link, comment_text, time, count):
+        cookies_for_bot1 = pickle.load(open("cookies_bot_1.pkl", "rb"))
+        cookies_for_bot2 = pickle.load(open("cookies_bot_2.pkl", "rb"))
+        cookies_for_bot3 = pickle.load(open("cookies_bot_3.pkl", "rb"))
+        bot1.get(link)
+        bot2.get(link)
+        bot3.get(link)
+        for cookie in cookies_for_bot1:
+            bot1.add_cookie(cookie)
+        for cookie in cookies_for_bot2:
+            bot2.add_cookie(cookie)
+        for cookie in cookies_for_bot3:
+            bot3.add_cookie(cookie)
+        who_is_he = [bot1, bot2, bot3]
+        # Переведём время в секунды
+        letter = time[len(time) - 1]
+        if letter == 'm':
+            time = int(time[0:len(time) - 1]) * 60
+        if letter == 's':
+            time = int(time[0:len(time) - 1]) * 1
+        if letter == 'h':
+            time = int(time[0:len(time) - 1]) * 3600
+        if letter == 'd':
+            time = int(time[0:len(time) - 1]) * 3600 * 24
+        sleep(5)
+
+        for i in range(count):
+            author = random.choice(who_is_he)
+            comment = author.find_element_by_class_name('Ypffh')
+            comment.click()
+            comment = author.find_element_by_class_name('Ypffh.focus-visible')
+            comment.send_keys(comment_text)
+            send_button = author.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button')
+            send_button.click()
+            sleep(time)
+        sleep(2)
+
+BOTS = instabot()
---
+++ telegram_bot.py
@@ -0,0 +1,100 @@
+from instabot import BOTS
+import aiogram.utils.markdown as md
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+
+
+bot = Bot(token='1530895721:AAHwJVW512nOzpo1bb-piaqUr10SMiqqFYg')
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+# создаём форму и указываем поля
+class Form(StatesGroup):
+    link = State()
+    text = State()
+    time = State()
+    count = State()
+
+# Добавляем возможность отмены, если пользователь передумал заполнять
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    await state.finish()
+    await message.reply('ОК')
+
+
+# Начинаем наш диалог
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await Form.link.set()
+    await message.reply("Привет! Укажи ссылку на пост")
+
+
+
+# Сюда приходит ответ с ссылкой
+@dp.message_handler(state=Form.link)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['link'] = message.text
+
+    await Form.next()
+    await message.reply("Какой будет текст комментария ?")
+
+
+# Принимаем текст и узнаём периодичность
+@dp.message_handler(state=Form.text)
+async def process_age(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['text'] = message.text
+    await Form.next()
+
+    await message.reply("Укажи переодичность")
+
+
+# Сохраняем пeреодичность, узнаём кол-во сообщений
+@dp.message_handler(state=Form.time)
+async def process_gender(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['time'] = message.text
+    await Form.next()
+
+    await message.reply("И было бы неплохо знать сколько сообщений надо отправить")
+
+
+# Сохраняем кол-во сообщений и запускаем скрипт
+@dp.message_handler(state=Form.count)
+async def process_gender(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['count'] = message.text
+    await Form.next()
+
+    await message.answer("готово")
+    await bot.send_message(
+        message.chat.id,
+        md.text(
+            md.text('link:', data['link']),
+            md.text('text:', data['text']),
+            md.text('time:', data['time']),
+            md.text('count:', data['count']),
+            sep='\n',
+        ),
+    )
+
+    await message.answer("Боты запущены, начинаю работу")
+    BOTS.type_comments(data['link'],data['text'],data['time'],int(data['count']))
+    await message.answer("Выполнено")
+    await state.finish()
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
